f6d516f5022ad2be4528409889aecfcc9eb9df65
DanielROC-dev <dkempers@roc-dev.com>
2020-05-11 03:54:57 +0200

    2.1

---
+++ vulmetjs.js
@@ -0,0 +1,4 @@
+document.getElementById("paginatitel").innerText = "Dit is de paginatitel"
+document.getElementById("subkop").innerText = "Dit is de subkop"
+document.getElementById("aap1").src = "https://avatars2.githubusercontent.com/u/17444754?s=400&u=9d99c6d680a00568b91c6b074a0a8600424fda7a&v=4"
+document.getElementById("aap2").src = "https://img.devrant.com/devrant/rant/r_565529_WRQcK.jpg"
